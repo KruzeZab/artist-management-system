bc9110b6e584dee229248a64bea1a231070001a5
Kruze Zab <kruzezab4@gmail.com>
2025-03-18 10:54:18 +0545

    add login and signup page

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,24 +1,25 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function loadComponent(selector: string, file: string) {
+  fetch(file)
+    .then((response) => response.text())
+    .then((html) => {
+      document.querySelector(selector)!.innerHTML = html;
+    })
+    .catch((error) => console.error(`Error loading ${file}:`, error));
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function loadPageStyles() {
+  const viewStyle = document.createElement('link');
+  viewStyle.rel = 'stylesheet';
+
+  viewStyle.href = './style.css';
+
+  document.head.appendChild(viewStyle);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadComponent('header', './components/header.html');
+  loadComponent('footer', './components/footer.html');
+  loadPageStyles();
+});
